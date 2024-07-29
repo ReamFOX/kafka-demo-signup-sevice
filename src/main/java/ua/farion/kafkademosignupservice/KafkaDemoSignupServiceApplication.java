b251fca8f153d1e532fe0d7a07969192457501b0
reamfox <sashafarion@gmail.com>
2024-07-29 10:36:54 +0300

    innit commit

---
+++ src/main/java/ua/farion/kafkademosignupservice/KafkaDemoSignupServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.farion.kafkademosignupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoSignupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoSignupServiceApplication.class, args);
+    }
+
+}
